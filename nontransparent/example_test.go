ba59e8d8e7b5dbb2fa3f0e514df77cb007c5de39
Leonardo Di Donato <leodidonato@gmail.com>
2025-01-25 19:06:30 +0100

    test(nontransparent): example showing how is now possible to pass down the underlying machine options
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- nontransparent/example_test.go
+++ nontransparent/example_test.go
@@ -1,13 +1,15 @@
 package nontransparent
 
 import (
-	"github.com/davecgh/go-spew/spew"
 	"io"
 	"math/rand"
 	"strings"
-
-	"github.com/leodido/go-syslog/v4"
 	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/leodido/go-syslog/v4"
+	"github.com/leodido/go-syslog/v4/rfc3164"
+	"github.com/leodido/go-syslog/v4/rfc5424"
 )
 
 func Example_withoutTrailerAtEnd() {
@@ -28,6 +30,50 @@ func Example_withoutTrailerAtEnd() {
 	// }
 }
 
+func Example_withCurrentYear_RFC3164() {
+	results := []syslog.Result{}
+	acc := func(res *syslog.Result) {
+		if res != nil {
+			// Force year to match the one in the comment below
+			x, _ := res.Message.(*rfc3164.SyslogMessage)
+			x.Timestamp = func(t1 *time.Time) *time.Time {
+				currentY := time.Now().Year()
+				t2 := t1.AddDate(2021-currentY, 0, 0)
+
+				return &t2
+			}(x.Timestamp)
+			newRes := syslog.Result{Message: x, Error: res.Error}
+			results = append(results, newRes)
+		}
+	}
+	// Notice the message ends without trailer but we catch it anyway
+	r := strings.NewReader("<13>Dec  2 16:31:03 host app: Test\n")
+	NewParserRFC3164(
+		syslog.WithListener(acc),
+		syslog.WithMachineOptions(rfc3164.WithYear(rfc3164.CurrentYear{})),
+	).Parse(r)
+	output(results)
+	// Output:
+	// ([]syslog.Result) (len=1) {
+	//  (syslog.Result) {
+	//   Message: (*rfc3164.SyslogMessage)({
+	//    Base: (syslog.Base) {
+	//     Facility: (*uint8)(1),
+	//     Severity: (*uint8)(5),
+	//     Priority: (*uint8)(13),
+	//     Timestamp: (*time.Time)(2021-12-02 16:31:03 +0000 UTC),
+	//     Hostname: (*string)((len=4) "host"),
+	//     Appname: (*string)((len=3) "app"),
+	//     ProcID: (*string)(<nil>),
+	//     MsgID: (*string)(<nil>),
+	//     Message: (*string)((len=4) "Test")
+	//    }
+	//   }),
+	//   Error: (error) <nil>
+	//  }
+	// }
+}
+
 func Example_bestEffortWithoutTrailerAtEnd() {
 	results := []syslog.Result{}
 	acc := func(res *syslog.Result) {
@@ -35,7 +81,7 @@ func Example_bestEffortWithoutTrailerAtEnd() {
 	}
 	// Notice the message ends without trailer but we catch it anyway
 	r := strings.NewReader("<1>1 2003-10-11T22:14:15.003Z host.local - - - - mex")
-	NewParser(syslog.WithBestEffort(), syslog.WithListener(acc)).Parse(r)
+	NewParser(syslog.WithMachineOptions(rfc5424.WithBestEffort()), syslog.WithListener(acc)).Parse(r)
 	output(results)
 	// Output:
 	// ([]syslog.Result) (len=1) {
@@ -66,7 +112,7 @@ func Example_bestEffortOnLastOne() {
 		results = append(results, *res)
 	}
 	r := strings.NewReader("<1>1 - - - - - - -\n<3>1\n")
-	NewParser(syslog.WithBestEffort(), syslog.WithListener(acc)).Parse(r)
+	NewParser(syslog.WithMachineOptions(rfc5424.WithBestEffort()), syslog.WithListener(acc)).Parse(r)
 	output(results)
 	// Output:
 	// ([]syslog.Result) (len=2) {
@@ -137,7 +183,7 @@ func Example_intoChannelWithLF() {
 		results <- x
 	}
 
-	p := NewParser(syslog.WithListener(ln), syslog.WithBestEffort())
+	p := NewParser(syslog.WithListener(ln), syslog.WithMachineOptions(rfc5424.WithBestEffort()))
 	go func() {
 		defer close(results)
 		defer r.Close()

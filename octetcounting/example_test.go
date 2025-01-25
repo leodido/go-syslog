34d2896b3b4b13e41e6b521b1afb23d9f849c2a0
Leonardo Di Donato <leodidonato@gmail.com>
2025-01-25 19:28:47 +0100

    test(octetcounting): example showing how to use rfc3164.WithYear(rfc3164.CurrentYear{})
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- octetcounting/example_test.go
+++ octetcounting/example_test.go
@@ -261,3 +261,104 @@ func Example_channelOpenBSD() {
 	//  Error: (error) <nil>
 	// }
 }
+
+func Example_channelOpenBSDWithCurrentYear() {
+	messages := []string{
+		"42 <46>Apr 28 11:53:44 syslogd[18823]: start\n",
+		"44 <47>Apr 28 11:53:44 syslogd[18823]: running\n",
+		"88 <86>Apr 28 11:53:46 doas: catap ran command ls / as root from /home/catap/src/go-syslog\n",
+	}
+
+	r, w := io.Pipe()
+
+	go func() {
+		defer w.Close()
+
+		for _, m := range messages {
+			w.Write([]byte(m))
+			time.Sleep(time.Millisecond * 220)
+		}
+	}()
+
+	c := make(chan syslog.Result)
+	emit := func(res *syslog.Result) {
+		c <- *res
+	}
+
+	parser := NewParserRFC3164(
+		syslog.WithMachineOptions(rfc3164.WithBestEffort(), rfc3164.WithYear(rfc3164.CurrentYear{})),
+		syslog.WithListener(emit),
+	)
+	go func() {
+		defer close(c)
+		parser.Parse(r)
+	}()
+
+	for r := range c {
+		mes := r
+		if r.Message != nil {
+			// Force year to match the one in the comment below
+			x := r.Message.(*rfc3164.SyslogMessage)
+			x.Timestamp = func(t1 *time.Time) *time.Time {
+				currentY := time.Now().Year()
+				t2 := t1.AddDate(2024-currentY, 0, 0)
+
+				return &t2
+			}(x.Timestamp)
+			mes = syslog.Result{Message: x, Error: r.Error}
+		}
+		output(mes)
+	}
+
+	r.Close()
+
+	// Output:
+	// (syslog.Result) {
+	//  Message: (*rfc3164.SyslogMessage)({
+	//   Base: (syslog.Base) {
+	//    Facility: (*uint8)(5),
+	//    Severity: (*uint8)(6),
+	//    Priority: (*uint8)(46),
+	//    Timestamp: (*time.Time)(2024-04-28 11:53:44 +0000 UTC),
+	//    Hostname: (*string)(<nil>),
+	//    Appname: (*string)((len=7) "syslogd"),
+	//    ProcID: (*string)((len=5) "18823"),
+	//    MsgID: (*string)(<nil>),
+	//    Message: (*string)((len=5) "start")
+	//   }
+	//  }),
+	//  Error: (error) <nil>
+	// }
+	// (syslog.Result) {
+	//  Message: (*rfc3164.SyslogMessage)({
+	//   Base: (syslog.Base) {
+	//    Facility: (*uint8)(5),
+	//    Severity: (*uint8)(7),
+	//    Priority: (*uint8)(47),
+	//    Timestamp: (*time.Time)(2024-04-28 11:53:44 +0000 UTC),
+	//    Hostname: (*string)(<nil>),
+	//    Appname: (*string)((len=7) "syslogd"),
+	//    ProcID: (*string)((len=5) "18823"),
+	//    MsgID: (*string)(<nil>),
+	//    Message: (*string)((len=7) "running")
+	//   }
+	//  }),
+	//  Error: (error) <nil>
+	// }
+	// (syslog.Result) {
+	//  Message: (*rfc3164.SyslogMessage)({
+	//   Base: (syslog.Base) {
+	//    Facility: (*uint8)(10),
+	//    Severity: (*uint8)(6),
+	//    Priority: (*uint8)(86),
+	//    Timestamp: (*time.Time)(2024-04-28 11:53:46 +0000 UTC),
+	//    Hostname: (*string)(<nil>),
+	//    Appname: (*string)((len=4) "doas"),
+	//    ProcID: (*string)(<nil>),
+	//    MsgID: (*string)(<nil>),
+	//    Message: (*string)((len=61) "catap ran command ls / as root from /home/catap/src/go-syslog")
+	//   }
+	//  }),
+	//  Error: (error) <nil>
+	// }
+}
